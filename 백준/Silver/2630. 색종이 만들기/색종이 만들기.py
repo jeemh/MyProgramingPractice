46ac6f636df56099a01910c09475e0b3332b8ea1
jeemh <144863392+jeemh@users.noreply.github.com>
2024-01-16 21:47:06 +0900

    [Silver II] Title: 색종이 만들기, Time: 92 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.py
@@ -0,0 +1,59 @@
+import sys
+
+global zeroCnt
+zeroCnt = 0
+global oneCnt
+oneCnt = 0
+
+def is_all_fill(arr):
+    n = arr[0][0]
+    for i in range(len(arr)):
+        for j in range(len(arr)):
+            if arr[i][j] != n:
+                return False
+    return True
+
+def copy_arr(arr,startX,startY,endX,endY):
+    returnArr = [[None] * (endY - startY + 1) for _ in range(endX - startX + 1)]
+    k = 0
+    for i in range(startX,endX+1):
+        l = 0
+        for j in range(startY,endY+1):
+            returnArr[k][l] = arr[i][j]
+            l += 1
+        k += 1
+    return returnArr
+
+def recursion(arr):
+    global zeroCnt
+    global oneCnt
+    if is_all_fill(arr):
+        if arr[0][0] == 1:
+            oneCnt += 1
+        else:
+            zeroCnt += 1
+    else:
+        copyArr = [None] * 4
+        
+        copyArr[0] = copy_arr(arr, 0, 0, len(arr)//2-1, len(arr)//2-1)
+        copyArr[1] = copy_arr(arr, 0, len(arr)//2, len(arr)//2-1, len(arr)-1)
+        copyArr[2] = copy_arr(arr, len(arr)//2, 0, len(arr)-1, len(arr)//2-1)
+        copyArr[3] = copy_arr(arr, len(arr)//2, len(arr)//2, len(arr)-1, len(arr)-1)
+        # copyArr[0] = arr[0:len(arr)//2, 0:len(arr)//2]
+        # copyArr[1] = arr[0:len(arr)//2, len(arr)//2:len(arr)-1]
+        # copyArr[2] = arr[len(arr)//2+1:len(arr)-1, 0:len(arr)//2]
+        # copyArr[3] = arr[len(arr)//2+1:len(arr)-1, len(arr)//2+1:len(arr)-1]
+        recursion(copyArr[0])
+        recursion(copyArr[1])
+        recursion(copyArr[2])
+        recursion(copyArr[3])
+
+
+n = int(input())
+arr = [None]*n
+for i in range(n):
+    arr[i] = list(map(int, input().split()))
+
+recursion(arr)
+print(zeroCnt)
+print(oneCnt)
