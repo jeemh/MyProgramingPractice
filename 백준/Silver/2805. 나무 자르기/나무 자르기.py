40df2eda24d5bd3da7e2b33e74b4b3e1dc2ab132
jeemh <144863392+jeemh@users.noreply.github.com>
2024-01-17 15:31:13 +0900

    [Silver II] Title: 나무 자르기, Time: 4116 ms, Memory: 143264 KB -BaekjoonHub

---
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -0,0 +1,34 @@
+import sys
+sys.setrecursionlimit(10**8)
+
+def sumOfCutTree(tree,standard):
+    sum = 0
+    for oneOfTree in tree:
+        if oneOfTree > standard:
+            sum += oneOfTree - standard
+        else:
+            break
+    return sum
+
+def binarySearch(tree, key, start, mid, end):
+    if sumOfCutTree(tree,mid) == key or start > end:
+        print(mid)
+        return
+    
+    elif sumOfCutTree(tree,mid) > key:
+        start = mid + 1
+        mid = (start + end)//2
+        
+    elif sumOfCutTree(tree,mid) < key:
+        end = mid - 1
+        mid = (start + end)//2
+        
+    binarySearch(tree, key, start, mid, end)
+
+n, m = map(int, sys.stdin.readline().split())
+tree = list(map(int, sys.stdin.readline().split()))
+
+maxTree = max(tree)
+tree.sort(reverse=True)
+# print(tree)
+binarySearch(tree, m, 0, maxTree//2, maxTree)
