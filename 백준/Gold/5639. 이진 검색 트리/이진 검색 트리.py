9d68ad753d103f247b58dfec760f86c638dfdcae
jeemh <144863392+jeemh@users.noreply.github.com>
2024-01-22 11:30:57 +0900

    [Gold V] Title: 이진 검색 트리, Time: 2156 ms, Memory: 33548 KB -BaekjoonHub

--- 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.py
+++ 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.py
@@ -1,28 +1,37 @@
 import sys
-sys.setrecursionlimit(10**6)
+sys.setrecursionlimit(10**9)
 
-def postorder(arr, startIndex, endIndex):
-    i = startIndex + 1
-    if endIndex - startIndex == 0:
-        print(arr[startIndex])
-        return 
-    if endIndex - startIndex == -1:
-        return
-    while i <= endIndex:
-        if arr[startIndex] < arr[i]:
-            break
-        i += 1
-    postorder(arr, startIndex+1, i-1)
-    postorder(arr, i, endIndex)
-    print(arr[startIndex])
+preorder_arr = []
 
-
-arr = []
-index = 0
+# 엔터 들어올 때까지 입력
 while True:
     try:
-        arr.append(int(sys.stdin.readline()))
+        preorder_arr.append(int(sys.stdin.readline().rstrip()))
     except:
         break
-    index += 1
-postorder(arr,0,index-1)
+
+def postorder(root_idx, end_idx):
+    if root_idx > end_idx:
+        return
+    
+    global preorder_arr
+    
+    # 만약 root보다 큰 값 없는 경우 전부 왼쪽 서브트리로 취급
+    right_start = end_idx + 1
+
+    for i in range(root_idx + 1, end_idx + 1):
+        if preorder_arr[root_idx] < preorder_arr[i]:
+            right_start = i
+            break
+    
+    # root 다음부터 왼쪽 서브트리 탐색
+    postorder(root_idx + 1, right_start - 1)
+
+    # 왼쪽 서브트리 탐색 끝나면 오른쪽 서브트리 탐색
+    postorder(right_start, end_idx)
+
+    # 왼쪽, 오른쪽 서브트리 탐색 끝나면 root 출력
+    print(preorder_arr[root_idx])
+
+
+postorder(0, len(preorder_arr) - 1)
