bb8c2f70d6ce3ccb0b50a9cdf02c23e620138e86
jeemh <144863392+jeemh@users.noreply.github.com>
2024-01-27 21:00:07 +0900

    [Gold V] Title: 동전, Time: 96 ms, Memory: 32208 KB -BaekjoonHub

--- 백준/Gold/9084. 동전/동전.py
+++ 백준/Gold/9084. 동전/동전.py
@@ -5,7 +5,7 @@ for _ in range(testCase):
     coins = list(map(int, input().split(' ')))
     targetAmount = int(input())
 
-    memo = [[0] * (targetAmount+1) for _ in range(len(coins)+1)]
+    memo = [[0] * (targetAmount+1) for _ in range(numOfCoin+1)]
     for coin in memo:
         coin[0] = 1
     
@@ -14,6 +14,6 @@ for _ in range(testCase):
             if row >= coins[col]:
                 memo[col][row] += memo[col][row-coins[col]]
         memo[col+1] = memo[col]
-    print(memo[len(coins)][targetAmount])
+    print(memo[numOfCoin][targetAmount])
 
     # 참고 블로그 : https://d-cron.tistory.com/23
