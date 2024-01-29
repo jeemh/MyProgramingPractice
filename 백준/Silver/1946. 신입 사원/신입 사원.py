71d106c35294de5ac1b7a0d014f164b0f9561b35
jeemh <144863392+jeemh@users.noreply.github.com>
2024-01-29 14:56:30 +0900

    [Silver I] Title: 신입 사원, Time: 3940 ms, Memory: 52684 KB -BaekjoonHub

--- 백준/Silver/1946. 신입 사원/신입 사원.py
+++ 백준/Silver/1946. 신입 사원/신입 사원.py
@@ -1,4 +1,19 @@
 import sys
+def sortAndDropOut(sortCriteria, criteriaForElimination, volunteer):
+
+    howManyDropOut = 0 
+    volunteer = sorted(volunteer, key=lambda x: x[sortCriteria]) #기준으로 정렬
+    minScore = volunteer[0][criteriaForElimination] #정렬된 스코어중 나머지 스코어로 비교하기위해 초기값을 0번째 지원자로 설정
+
+    for i in range(1,len(volunteer)):
+        if volunteer[i][criteriaForElimination] > minScore and volunteer[i][2]:
+            volunteer[i][2] = 0 #면접 탈락
+            howManyDropOut += 1
+
+        elif volunteer[i][criteriaForElimination] < minScore: #최고등수 초기화
+            minScore = volunteer[i][criteriaForElimination]
+    
+    return howManyDropOut
 
 testCase = int(sys.stdin.readline())
 for _ in range(testCase):
@@ -8,22 +23,23 @@ for _ in range(testCase):
     for i in range(N):
         volunteer[i] = list(map(int,sys.stdin.readline().split()))
         volunteer[i].append(1)
-    volunteer.sort()
-    minScore = volunteer[0][1]
-    for i in range(1,len(volunteer)):
-        # print("<",volunteer[i][1], minScore,">")
-        if volunteer[i][1] > minScore :
-            volunteer[i][2] = 0 #면접 탈락
-            cnt -= 1
-        elif volunteer[i][1] < minScore:
-            minScore = volunteer[i][1]
-    volunteer = sorted(volunteer, key=lambda x: x[1])        
-    minScore = volunteer[0][0]
-    for i in range(1,len(volunteer)):
-        if volunteer[i][0] > minScore and volunteer[i][2]:
-            volunteer[i][2] = 0 #면접 탈락
-            cnt -= 1
-        elif volunteer[i][0] < minScore:
-            minScore = volunteer[i][0]
+    cnt -= sortAndDropOut(0,1,volunteer)
+    cnt -= sortAndDropOut(1,0,volunteer)
+    # volunteer.sort()
+    # minScore = volunteer[0][1]
+    # for i in range(1,len(volunteer)):
+    #     if volunteer[i][1] > minScore :
+    #         volunteer[i][2] = 0 #면접 탈락
+    #         cnt -= 1
+    #     elif volunteer[i][1] < minScore:
+    #         minScore = volunteer[i][1]
+    # volunteer = sorted(volunteer, key=lambda x: x[1])        
+    # minScore = volunteer[0][0]
+    # for i in range(1,len(volunteer)):
+    #     if volunteer[i][0] > minScore and volunteer[i][2]:
+    #         volunteer[i][2] = 0 #면접 탈락
+    #         cnt -= 1
+    #     elif volunteer[i][0] < minScore:
+    #         minScore = volunteer[i][0]
     print(cnt)
     #print(volunteer)
